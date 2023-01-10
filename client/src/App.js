ca41bca2ebd272a251f4ed92871e527bedcf1091
dobbinsi <iandobbins5@gmail.com>
2023-01-10 00:39:07 -0800

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import fscube from "./logos/fscubewhite.png";
+import Typewriter from "typewriter-effect";
+import Tilt from "react-parallax-tilt";
 
 function App() {
+  const [flipVertically, setFlipVertically] = useState(false);
+  const flipHandler = () => {
+    setFlipVertically(!flipVertically);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Tilt
+        className="parallax-effect-img"
+        tiltReverse={true}
+        trackOnWindow={true}
+        gyroscope={true}
+        perspective={500}
+        scale={1.05}
+        flipVertically={flipVertically}
+      >
+        <div className="glass" onClick={flipHandler}>
+          <a
+            href="https://flipsidecrypto.xyz/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <img src={fscube} alt="flipside" className="fslogo" />
+          </a>
+        </div>
+      </Tilt>
+      <div className="typewriter">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString("Flipside Governance").start();
+          }}
+          options={{
+            wrapperClassName: "type",
+            cursorClassName: "type-cursor",
+          }}
+        />
+      </div>
+      <div className="links">
+        <a>Who We Are</a>
+        <a>Delegate Platform</a>
+        <a>Research & Content</a>
+      </div>
     </div>
   );
 }
